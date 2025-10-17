6bcad020dc46b4f26dc8503820c2435df3235b8e
Cam Bitter <45082820+CamBitter@users.noreply.github.com>
2025-10-17 12:43:18 -0400

    Revert "Add supabase + initial DB schema"

--- supabase/schemas/event_tag_assignments.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE event_tag_assignments (
-    event_id BIGINT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
-    tag_id BIGINT NOT NULL REFERENCES event_tags(id) ON DELETE CASCADE,
-    PRIMARY KEY (event_id, tag_id)
-);
-
-CREATE INDEX ON event_tag_assignments(event_id); -- Efficient selection of an event's tags
-CREATE INDEX ON event_tag_assignments(tag_id); -- Efficient selection of all the events that have a particular tag
