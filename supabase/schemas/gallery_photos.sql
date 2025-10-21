9a67d07d9c9cb2885f1d898eed759e7de1249346
Toby Penner <tobypenner01@gmail.com>
2025-10-21 14:24:16 -0400

    Add supabase + database structure

---
+++ supabase/schemas/gallery_photos.sql
@@ -0,0 +1,6 @@
+CREATE TABLE gallery_photos (
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    photo_path TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (user_id, photo_path)
+);
