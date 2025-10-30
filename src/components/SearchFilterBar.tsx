f4d49fc4b4a77fa17051892305a20c33d3f57006
Cam Bitter <ccabitter@gmail.com>
2025-10-30 15:17:43 -0400

    first commit

---
+++ src/components/SearchFilterBar.tsx
@@ -0,0 +1,8 @@
+/*
+ * Author: Cam Bitter
+ * Description:
+ *  This component will call a callback function with arguments
+ *  that show the current state of the search and filter parameters
+ */
+
+export default function SearchFilterBar() {}
