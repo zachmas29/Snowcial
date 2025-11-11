9b02f3bc830484c4c13fbac87b825c63f47ecb72
Cam Bitter <ccabitter@gmail.com>
2025-11-11 14:08:43 -0500

    fix linter errors

--- src/components/TagSelector.tsx
+++ src/components/TagSelector.tsx
@@ -1,16 +1,20 @@
 import { Autocomplete, TextField } from "@mui/material";
-import type { Tables } from "@/types/database.types";
 import type { GenericTagType } from "@/types/EventCreator.types";
 
 interface TagSelectorProps {
   availableTags: GenericTagType[];
   selectedTags: GenericTagType[];
   setSelectedTags: (tags: GenericTagType[]) => void;
-};
+}
 
-export default function TagSelector({ availableTags, selectedTags, setSelectedTags}: TagSelectorProps) {
-  return <div>
-    <Autocomplete
+export default function TagSelector({
+  availableTags,
+  selectedTags,
+  setSelectedTags,
+}: TagSelectorProps) {
+  return (
+    <div>
+      <Autocomplete
         multiple
         id="tags-standard"
         options={availableTags}
@@ -19,13 +23,14 @@ export default function TagSelector({ availableTags, selectedTags, setSelectedTa
         onChange={(_event, newValue) => setSelectedTags(newValue)}
         isOptionEqualToValue={(option, value) => option.id === value.id} // prevents duplicates
         renderInput={(params) => (
-        <TextField
+          <TextField
             {...params}
             variant="standard"
             label="Add Event Tags"
             placeholder="Tags"
-        />
+          />
         )}
-    />
+      />
     </div>
+  );
 }
--- src/types/EventCreator.types.ts
+++ src/types/EventCreator.types.ts
@@ -1,14 +1,12 @@
-import type { Tables } from "./database.types";
-
 export interface EventFormData {
-    title: string,
-    description: string,
-    // biome-ignore lint/style/useNamingConvention: <Supabase wants snake_case>
-    event_time: Date | null,
-    tags: GenericTagType[] | [];
+  title: string;
+  description: string;
+  // biome-ignore lint/style/useNamingConvention: <Supabase wants snake_case>
+  event_time: Date | null;
+  tags: GenericTagType[] | [];
 }
 
 export interface GenericTagType {
-    id: number,
-    name: string
+  id: number;
+  name: string;
 }
