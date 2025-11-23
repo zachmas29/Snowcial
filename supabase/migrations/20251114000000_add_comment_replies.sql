fb972d02c1877684d5997abb5f45f493a51a2c51
Ned Cutler <nedcut@gmail.com>
2025-11-23 13:25:47 -0500

    removed constraint causing error

--- supabase/migrations/20251114000000_add_comment_replies.sql
+++ supabase/migrations/20251114000000_add_comment_replies.sql
@@ -3,12 +3,3 @@ alter table public.event_comments
 
 create index if not exists event_comments_parent_comment_id_idx
   on public.event_comments(parent_comment_id);
-
-alter table public.event_comments
-  add constraint event_comments_parent_same_event
-  check (
-    parent_comment_id is null
-    or event_id = (
-      select event_id from public.event_comments parent where parent.id = parent_comment_id
-    )
-  );
